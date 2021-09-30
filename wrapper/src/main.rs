b650083bcbb668c1c36998e47970cbc3b555927b
Evan Pratten <ewpratten@gmail.com>
2021-09-30 15:57:26 -0400

    slightly edit log output

--- wrapper/src/main.rs
+++ wrapper/src/main.rs
@@ -1,15 +1,21 @@
-use game::{GameConfig, StaticGameData, game_begin};
+use game::{game_begin, GameConfig, StaticGameData};
 
 #[tokio::main]
 async fn main() {
     // Enable logging
-    tracing_subscriber::fmt::init();
+    tracing_subscriber::fmt()
+        .compact()
+        .with_thread_names(true)
+        .with_ansi(true)
+        .with_max_level(tracing::Level::INFO)
+        .init();
 
     // Load the general config for the game
     // This happens here so we can properly track sentry events
     let game_config = GameConfig::load(
         StaticGameData::get("configs/application.json").expect("Failed to load application.json"),
-    ).unwrap();
+    )
+    .unwrap();
 
     // Connect to sentry
     let _sentry_guard = sentry::init((
