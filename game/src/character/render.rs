0d5be8759ac7274714dd792dc32787262cc80491
Evan Pratten <ewpratten@gmail.com>
2021-10-03 14:25:34 -0400

    new character asset

--- game/src/character/render.rs
+++ game/src/character/render.rs
@@ -22,7 +22,7 @@ pub fn render_character_in_camera_space(
 
     // Calculate the frame ID to render
     let frame_id = match player.current_state {
-        crate::character::CharacterState::Jumping => 4,
+        crate::character::CharacterState::Jumping => 6,
         _ => (frames_since_state_change % player.sprite_sheet.sprite_count as f32).floor() as usize,
     };
 
