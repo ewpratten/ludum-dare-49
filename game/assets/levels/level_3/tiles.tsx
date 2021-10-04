1f8ca262d26e406a830ec31baa65b3048e936d4a
Marcelo Geldres <geldres.marcelo@gmail.com>
2021-10-04 01:11:42 -0400

    Level_3

---
+++ game/assets/levels/level_3/tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="tiles" tilewidth="64" tileheight="64" tilecount="374" columns="22">
+ <image source="C:/Users/user/Pictures/ld49/tile1.png" trans="000000" width="1456" height="1088"/>
+</tileset>
